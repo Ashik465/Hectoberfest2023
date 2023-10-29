8db087952b6445d1274d9814bcd3553560728589
MD Ashikul Islam <helloashikul@gmail.com>
2023-10-30 02:22:56 +0600

    Create Merge.py

---
+++ Merge.py
@@ -0,0 +1,38 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        # Merge the two sorted halves into the original array
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # Check for any remaining elements in left and right halves
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+    return arr
+
+# Example usage:
+my_list = [12, 11, 13, 5, 6, 7]
+sorted_list = merge_sort(my_list)
+print("Sorted array:", sorted_list)
